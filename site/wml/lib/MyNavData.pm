2e062e9c5a7b7332b69f79d0f93d95ea718f0361
Shlomi Fish <shlomif@shlomifish.org>
2005-03-11 20:38:43 +0000

    Importing the current site into its directory

---
+++ site/wml/lib/MyNavData.pm
@@ -0,0 +1,53 @@
+package MyNavData;
+
+my $hosts =
+{
+    'berlios' => 
+    {
+        'base_url' => "http://jmikmod.berlios.de/",
+    },
+};
+
+my $tree_contents =
+{
+    'host' => "berlios",
+    'text' => "MikMod for Java",
+    'title' => "MikMod for Java - a multi-format Module Player for the Java Environment",
+    'subs' =>
+    [
+        {
+            'text' => "Home",
+            'url' => "",
+        },
+        {
+            'text' => "About",
+            'url' => "about.html",
+            'title' => "About this Site",
+        },
+        {
+            'text' => "Download",
+            'url' => "download/",
+            'title' => "Download the code for the Site",
+        },
+        {
+            'separator' => 1,
+            'skip' => 1,
+        },
+        {
+            'url' => "links/",
+            'text' => "Links",
+            'title' => "Related Links",
+        },
+    ],
+};
+
+sub get_params
+{
+    return 
+        (
+            'hosts' => $hosts,
+            'tree_contents' => $tree_contents,
+        );
+}
+
+1;
